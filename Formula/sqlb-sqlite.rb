7e9df6b20f5fe6bf3ee16e4cf60719469e258ee4
SeongTae Jeong <seongtaejg@gmail.com>
2025-03-03 22:59:43 +0900

    Remove bottle section

--- Formula/sqlb-sqlite.rb
+++ Formula/sqlb-sqlite.rb
@@ -6,10 +6,6 @@ class SqlbSqlite < Formula
   sha256 "ac992f7fca3989de7ed1fe99c16363f848794c8c32a158dafd4eb927a2e02fd5"
   license "blessing"
 
-  bottle do
-    root_url "https://github.com/lucydodo/homebrew-tap/releases/download/sqlb-sqlite-3.48.0"
-    sha256 cellar: :any, arm64_sonoma: "90e6a864154e6089ff1cfaacc5e589efae5cc844b47369174a8ebb04039f843b"
-  end
   env :std
 
   livecheck do
